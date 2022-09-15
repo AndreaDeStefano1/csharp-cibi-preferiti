0e500ed86e1e1429d1a2475196426e7c50d9e6b2
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-09-15 17:26:56 +0200

    Agggiunti spazi righe vuote

--- Program.cs
+++ Program.cs
@@ -8,19 +8,27 @@ string[] classificaCibi = { "Margherita", "Genovese", "Ragù", "Amatriciana", "B
 // string[] classificaCibi = { "Margherita", "Genovese", "Ragù",  "Bolognese", "Pesto" }; 
 
 Console.WriteLine("La lunghezza della classifica è " + classificaCibi.Length);
+Console.WriteLine();
+Console.WriteLine("La classifica è: ");
+
 
 for (int i = 0; i < classificaCibi.Length; i++)
 {
     Console.WriteLine((i + 1) + " - " + classificaCibi[i]);
 }
 
+Console.WriteLine();
+
 
 Console.WriteLine("Il mio cibo preferito top è " + classificaCibi[0]);
 
+Console.WriteLine();
+
 Console.WriteLine("Il mio cibo preferito non top è " + classificaCibi[classificaCibi.Length - 1]);
 
+Console.WriteLine();
 
-if(classificaCibi.Length % 2 != 0)
+if (classificaCibi.Length % 2 != 0)
 {
     Console.WriteLine("La posizione centrale della classifica è " + classificaCibi[classificaCibi.Length / 2]);
 }else
