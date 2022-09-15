1b9a2822bc88b065c4680621b6768d3e14786f86
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-09-15 15:30:26 +0200

    Stampato cibo di metà classifica

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] classificaCibi = { "Margherita", "Genovese", "Ragù", "Bolognese", "Pesto" };
+
+// array pari
+string[] classificaCibi = { "Margherita", "Genovese", "Ragù", "Amatriciana", "Bolognese", "Pesto" }; 
+
+// array dispari
+// string[] classificaCibi = { "Margherita", "Genovese", "Ragù",  "Bolognese", "Pesto" }; 
 
 Console.WriteLine("La lunghezza della classifica è " + classificaCibi.Length);
 
@@ -13,4 +18,16 @@ for (int i = 0; i < classificaCibi.Length; i++)
 Console.WriteLine("Il mio cibo preferito top è " + classificaCibi[0]);
 
 Console.WriteLine("Il mio cibo preferito non top è " + classificaCibi[classificaCibi.Length - 1]);
+
+
+if(classificaCibi.Length % 2 != 0)
+{
+    Console.WriteLine("La posizione centrale della classifica è " + classificaCibi[classificaCibi.Length / 2]);
+}else
+{
+    Console.WriteLine("Le posizioni centrali della classifica sono " + classificaCibi[classificaCibi.Length / 2] + " e " + classificaCibi[(classificaCibi.Length / 2) - 1]);
+
+}
+
+
 
