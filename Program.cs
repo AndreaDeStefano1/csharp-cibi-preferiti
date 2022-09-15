2fa7fcb79295e5f74c3adeaaff4c25f21527be34
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-09-15 15:22:57 +0200

    Esercizio iniziale

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string[] classificaCibi = { "Margherita", "Genovese", "Ragù", "Bolognese", "Pesto" };
+
+Console.WriteLine("La lunghezza della classifica è " + classificaCibi.Length);
+
+for (int i = 0; i < classificaCibi.Length; i++)
+{
+    Console.WriteLine(i + " - " + classificaCibi[i]);
+}
+
+
+Console.WriteLine("Il mio cibo preferito top è " + classificaCibi[0]);
+
+Console.WriteLine("Il mio cibo preferito non top è " + classificaCibi[classificaCibi.Length - 1]);
+
